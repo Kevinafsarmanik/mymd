1978d49fa084c520331c6aaa85f943e1f1970317
Vinzzt <97930168+Kevinafsarmanik@users.noreply.github.com>
2022-04-11 23:45:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = ''
-  var sticker_author = '               ig\n🔥 @rasel.ganz 🔥'
+  var sticker_author = '               ig\n🔥 @Kevinafsar_ 🔥'
  } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -67,7 +67,7 @@ const cpack = fs.readFileSync("lib/colong.json")
 const ctickerpack = JSON.parse(cpack)
 if (ctickerpack.cpackname == '') {
   var colong_name = 'sikilinsut olang nylong stikell'
-  var colong_author = 'Ciss 📸\n\n@rasel.ganz'
+  var colong_author = 'Ciss 📸\n\n@kevin.ganz'
  } else {
   var colong_name = ctickerpack.cpackname
   var colong_author = ctickerpack.cauthor
@@ -84,32 +84,32 @@ global.pclg = colong_name
 global.aclg = colong_author
 //======================================//
 
-global.wm = '@rasel.ganz'
+global.wm = '@kevinafsar_'
 global.ucpn = ''
 
 //================= Url =================//
-global.linkgc = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
-global.linkig = 'https://www.instagram.com/p/CH1A1c9J5pY/?utm_medium=copy_link'
-global.linkyt = ''
-global.linkfb = 'https://fb.watch/aYv0jAffAO'
-global.web = 'https://raselcomel.github.io'
-global.web1 = 'https://anu.rasell.repl.co'
+global.linkgc = 'https://chat.whatsapp.com/JWVl9J1sIxMG9PwyIMyQN6'
+global.linkig = 'https://www.instagram.com/kevinafsar_'
+global.linkyt = 'https://kevinafsarmanik.github.io/nino-wife.github.io/'
+global.linkfb = 'https://www.facebook.com/kevin.afsar.90'
+global.web = 'https://kevinafsarmanik.github.io/web-bucin/'
+global.web1 = 'https://kevinafsarmanik.github.io/bucinadeep/'
 
 global.deslink = ''
 global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@rasel.ganz'
+global.bodlink = '@kevinafsar_'
 
 //========= Url Template Button =========//
 global.dtu = 'Group Bot WhatsApp'
-global.urlnya = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
+global.urlnya = 'https://chat.whatsapp.com/JWVl9J1sIxMG9PwyIMyQN6'
 
 //========= Call Template Button =========//
 global.dtc = 'Add Me'
-global.nmbrnya = '+62 822-5604-8971'
+global.nmbrnya = '+62 831-3121-4121'
 
 
 //============ Proses Bot ============//
-global.wait = 'Tunggu sebentar ya sayang'
+global.wait = 'Tunggu sebentar sedang di proses ⌛'
 global.eror = 'Maaf terjadi kesalahan, silahkan coba lagi nanti'
 
 
